82a223444f0b77c2ff99ab9f8054462fa93b899c
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2023-03-08 04:27:13 +0900

    update 37

--- mysite/common/urls.py
+++ mysite/common/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
     # contrib.auth 앱의 LoginView 클래스를 활용해서 별도의 views.py 파일 수정 안해도 됨.
     # as_view 설정으로 registration디렉터리가 아닌 common 디렉터리에서 login.html파일을 참조한다.
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- mysite/config/settings.py
+++ mysite/config/settings.py
@@ -128,3 +128,4 @@ STATICFILES_DIRS = [
 ]
 # 로그인 성공 시 자동으로 이동할 URL
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
