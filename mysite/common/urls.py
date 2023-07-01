fd35962d6e7f33e106f9a3cbd8143103f053881e
ParkJuhyun <wngus6429@gmail.com>
2023-07-01 17:11:16 +0900

    Update urls.py

--- mysite/common/urls.py
+++ mysite/common/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = 'common'
 
 urlpatterns = [
-    # contrib.auth 앱의 LoginView 클래스를 활용해서 별도의 백업views.py 파일 수정 안해도 됨.
+    # contrib.auth 앱의 LoginView 클래스를 활용해서 별도의 백업 views.py 파일 수정 안해도 됨.
     # as_view 설정으로 registration디렉터리가 아닌 common 디렉터리에서 login.html파일을 참조
     path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
