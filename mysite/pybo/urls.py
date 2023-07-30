79f77b0fe558b0a7ee7f58a85859ee8cfc51ae7c
ParkJuhyun <wngus6429@gmail.com>
2023-07-30 19:32:15 +0900

    update study 230730

--- mysite/pybo/urls.py
+++ mysite/pybo/urls.py
@@ -22,10 +22,10 @@ urlpatterns = [
     path('comment/create/answer/<int:answer_id>/', comment_views.comment_create_answer, name='comment_create_answer'),
     path('comment/modify/answer/<int:comment_id>/', comment_views.comment_modify_answer, name='comment_modify_answer'),
     path('comment/delete/answer/<int:comment_id>/', comment_views.comment_delete_answer, name='comment_delete_answer'),
-
     # vote_views.py
     path('vote/question/<int:question_id>/', vote_views.vote_question, name='vote_question'),
     path('vote/answer/<int:answer_id>/', vote_views.vote_answer, name='vote_answer'),
+    
     # views 파일 안에 index 함수를 참조 한다고
     # 베이스
     # path('', views.index, name='index'),
